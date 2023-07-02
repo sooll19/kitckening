39cad4dcf09f19e6dfbf950e7e9709e49f0c381d
unknown <solvargas094@gmail.com>
2023-07-01 22:35:09 -0300

    kickening

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3030;
+
+app.use(express.static('public'))
+
+/*rutas*/
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "home.html"))
+);
+app.get("/*",(req,res) => 
+res.sendFile(path.join(__dirname,"views","404notFound.html")))
+app.listen(PORT, () =>
+  console.log(`Server running in http://localhost:${PORT}`)
+);
